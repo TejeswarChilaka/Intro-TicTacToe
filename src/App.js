a3a644784b2ce073cd8a2b6011efe94c103d3e60
TejeswarChilaka <chilaka.tejeswar@gmail.com>
2024-02-20 18:20:33 +0530

    Created MyButton Component.Nested it inside Component.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <p>gjjhgghghhggggggg,wyuyjhssssssssssk.hgsjhgdhjkd,hdsjkjkhhhsjkjkd,rgghkjgdsfgjkjhgfdsdd</p>
+    
+    </>
+  );
+}
+
+export default App;
