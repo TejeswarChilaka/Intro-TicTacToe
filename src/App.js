1ffbbf56a547467790357b03928250f55075d314
TejeswarChilaka <chilaka.tejeswar@gmail.com>
2024-02-21 13:28:45 +0530

    Addition of image

--- src/App.js
+++ src/App.js
@@ -9,13 +9,57 @@ function MyButton() {
   );
 }
 
-function App() {
+
+function AboutPage() {
+  let y = "Vue";
   return (
     <>
+   
+      <h1>Hello React</h1>
+      <p>Starting with Components...</p>
+      {y}
+    </>
+  );
+}
+
+const user = {
+  name :"Tejeswar Reddy",
+  role :"Software",
+  imageUrl : "assets/wallpaper-946.jpg",
+  imageSize :90,
+};
+
+function MyImg(){
+  return(
+    <>
+    <h2>My image</h2>
+    <img
+    className="avatar"
+    src={user.imageUrl}
+    alt = {'photo of'+user.name}
+    style={{
+      width: user.imageSize,
+      height: user.imageSize
+    }}/>
+    
+    
+    </>
+  )
+  
+};
+
+function App() {
+  let x = "Angular";
+  return (
+    <>
+    {x}
+   <MyImg></MyImg>
+      <AboutPage />
       <p>Hello Button!!</p>
       <MyButton />
     </>
   );
 }
+
 
 export default App;
