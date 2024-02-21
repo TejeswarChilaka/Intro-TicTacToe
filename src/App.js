33b8475559e3c7631a846e0304a2d96517484e7d
TejeswarChilaka <chilaka.tejeswar@gmail.com>
2024-02-21 19:07:52 +0530

    Sharing the Data between Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 // import logo from './logo.svg';
+import image from './image.jpg';
 import "./App.css";
+import { useState } from 'react';
 
-function MyButton() {
-  return (
-    <>
-      <button>MyButton</button>
-    </>
-  );
-}
 
 
 function AboutPage() {
-  let y = "Vue";
   return (
     <>
-   
       <h1>Hello React</h1>
       <p>Starting with Components...</p>
-      {y}
     </>
   );
 }
@@ -29,10 +21,18 @@ const user = {
   imageSize :90,
 };
 
+
+const user1 = {
+  name :"Shiva",
+  role :"Destroyer",
+  imageUrl : image ,
+  imageSize :90,
+};
+
 function MyImg(){
   return(
     <>
-    <h2>My image</h2>
+    <h2>My images</h2>
     <img
     className="avatar"
     src={user.imageUrl}
@@ -41,25 +41,84 @@ function MyImg(){
       width: user.imageSize,
       height: user.imageSize
     }}/>
-    
-    
+    <img
+    src={user1.imageUrl}
+    alt = {'photo of'+user1.name}
+    style={{
+      width: user1.imageSize,
+      height: user1.imageSize
+    }}/>
     </>
-  )
-  
+  ) 
 };
 
-function App() {
-  let x = "Angular";
+
+
+function condition(){  
+  let xy;
+  let y = false;
+  if (y){
+    xy = <MyButton />;
+  }else{
+    xy = <MyImg></MyImg>;
+  }
+  return(
+    <>
+    {xy}
+    </>
+  )
+}
+function uList(){
+  const products = [
+    {title : "phone",id : 1},
+    {title : "mobile",id : 2},
+    {title : "laptop",id : 3},
+    {title : "mouse",id : 4},
+  ]
+   
+  const listItems = products.map(product =>
+    <li key ={product.id}>
+      {product.id},
+       {product.title}
+    </li>
+    )
+    return (
+      <ul>{listItems}</ul>
+    )
+}
+
+
+function MyButton({count,onClick}) {
+  
   return (
     <>
-    {x}
-   <MyImg></MyImg>
-      <AboutPage />
-      <p>Hello Button!!</p>
-      <MyButton />
+      <button onClick = {onClick}>MyButton {count} times</button>
     </>
   );
 }
 
+function MyCustomButton(){
+  const [count,setCount] = useState(0);
+  function handleClick(){
+     setCount(count+1)
+  }
+  return(
+    <>
+    <MyButton count = {10} onClick ={handleClick}/>
+    <MyButton count = {20} onClick ={handleClick}/>
+    </>
+  )
+  
+}
+
+
+
+function App() {
+  
+  return (<>
+    <MyCustomButton/>
+   </>
+  )
+}
 
 export default App;
